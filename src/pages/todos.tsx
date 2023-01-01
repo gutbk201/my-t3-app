7c355cd59c170b14edf0ecb4d8632b4ca246b985
gutbk201 <siuym4mym4@gmail.com>
2023-01-01 11:03:08 +0000

    TODO, create, read, delete(update)

--- src/pages/todos.tsx
+++ src/pages/todos.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from "react";
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
 
@@ -5,12 +6,44 @@ import { trpc } from "../utils/trpc";
 
 const Todos: NextPage = () => {
   // const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+  const [userText, setUserText] = useState("");
   const todos = trpc.todo.getAll.useQuery();
-  console.log(todos.data);
+  const postOne = trpc.todo.postOne.useMutation();
+  const removeOne = trpc.todo.removeOne.useMutation();
+  const onTest = async () => {
+    // const onSuccess = (data) => todos.refetch();
+    postOne.mutate(
+      { text: userText },
+      {
+        onSuccess: (data) => {
+          console.log(data);
+          todos.refetch();
+        },
+      }
+    );
+    setUserText("");
+  };
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserText(e.target.value);
+  };
+  const onDelete = (id: string) => {
+    removeOne.mutate({ id }, { onSuccess: () => todos.refetch() });
+  };
   return (
     <main className="">
       <h2>Todos</h2>
-      <button className="rounded-md bg-main-b p-2 text-center">Test</button>
+      <input onChange={onChange} value={userText} />
+      <button className="rounded-md bg-main-b p-2 text-center" onClick={onTest}>
+        Test
+      </button>
+      <hr />
+      {todos.data
+        ?.filter((todo) => todo.hidden === "FALSE")
+        .map(({ id, text, user }) => (
+          <div key={id}>
+            <button onClick={() => onDelete(id)}>X</button> {user.name} : {text}
+          </div>
+        ))}
     </main>
   );
 };
--- src/server/trpc/router/todo.ts
+++ src/server/trpc/router/todo.ts
@@ -3,14 +3,60 @@ import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
 
 export const todoRouter = router({
-  // hello: publicProcedure
-  //   .input(z.object({ text: z.string().nullish() }).nullish())
-  //   .query(({ input }) => {
-  //     return {
-  //       greeting: `Hello ${input?.text ?? "world"}`,
-  //     };
-  //   }),
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.todo.findMany();
+    // ctx.prisma.todo.create({
+    //   data:{
+    //     text:'123',
+    //     user:{
+
+    //     }
+    //   }
+    // })
+    return ctx.prisma.todo.findMany({
+      select: {
+        id: true,
+        text: true,
+        hidden: true,
+        createdAt: true,
+        user: {
+          select: {
+            name: true,
+          },
+        },
+      },
+      // orderBy: [
+      //   {
+      //     role: 'desc',
+      //   },
+      // ],
+    });
   }),
+  postOne: publicProcedure
+    .input(
+      z.object({
+        text: z.string().min(1),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const userId = ctx.session?.user?.id || "";
+      const createdTodo = await ctx.prisma.todo.create({
+        data: {
+          text: input.text,
+          // userId,
+          user: {
+            connect: { id: userId },
+          },
+        },
+      });
+      return createdTodo;
+    }),
+  removeOne: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const updatedTodo = await ctx.prisma.todo.update({
+        where: { id: input.id },
+        data: { hidden: "TRUE" },
+      });
+      return updatedTodo;
+    }),
 });
