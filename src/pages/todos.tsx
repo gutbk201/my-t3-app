28f581b68f9da014b14580fc0dac8a4754f613a6
gutbk201 <siuym4mym4@gmail.com>
2023-01-12 02:07:48 +0000

    replace unAuthRedirection for pages auth

--- src/pages/todos.tsx
+++ src/pages/todos.tsx
@@ -2,25 +2,24 @@ import { FormEvent, ChangeEvent, useState } from "react";
 import { trpc } from "../utils/trpc";
 import { useTodoTrpc } from "hooks/useTodoTrpc";
 import type { GetServerSideProps, NextPage } from "next";
-import { unstable_getServerSession } from "next-auth/next";
-import { authOptions, unAuthRedirection } from "pages/api/auth/[...nextauth]";
+import { unAuthRedirection } from "pages/api/auth/[...nextauth]";
+import { getServerAuthSession } from "server/common/get-server-auth-session";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const session = await unstable_getServerSession(
-    ctx.req,
-    ctx.res,
-    authOptions
-  );
+  // const session = await unstable_getServerSession(
+  //   ctx.req,
+  //   ctx.res,
+  //   authOptions
+  // );
+  const session = await getServerAuthSession({ req: ctx.req, res: ctx.res });
   if (!session) return unAuthRedirection;
-  return { props: { session } };
+  return { props: {} };
 };
 
 const Todos: NextPage = () => {
   const [userText, setUserText] = useState("");
   const todos = trpc.todo.getAll.useQuery();
   const { addTodo, removeTodo } = useTodoTrpc();
-  // const { data: session } = useSession();
-  // console.log(session);
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     addTodo({ text: userText });
